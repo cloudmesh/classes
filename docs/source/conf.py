b3b9323ccb6c98e80fca84eaf0f251dc8afe7049
Hyungro Lee <hroe.lee@gmail.com>
2016-01-14 14:22:56 -0500

    restore to default rtd theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -129,7 +129,7 @@ html_theme = 'default'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_style = 'css/my_theme.css'
+#html_style = 'css/my_theme.css'
 
 #html_context = {
 #     'css_files': [
