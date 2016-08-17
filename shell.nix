fa54abb3b9548305f21934006b34ac87b8ed1b70
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-08-17 15:00:52 -0400

    new:dev: add shell.nix for repo

---
+++ shell.nix
@@ -0,0 +1,30 @@
+with import <nixpkgs> {};
+
+with pkgs;
+
+let
+
+  pythonTools = with python27Packages; [
+    python27Full
+    pip
+    virtualenv
+    ipython
+    numpy
+    scipy
+    pandas
+  ];
+
+  nbis = callPackage ./nbis.nix {};
+
+in
+
+stdenv.mkDerivation {
+  name = "python-lesson-1";
+  buildInputs = [
+    pythonTools
+  ];
+  shellHook = ''
+    test -d venv || virtualenv venv
+    source venv/bin/activate
+  '';
+}
