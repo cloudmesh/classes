a67e0cc889c6a13f6eb006176b814582c3213234
Gregor von Laszewski <laszewski@gmail.com>
2016-12-18 09:24:19 -0500

    new:dev: savety

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -134,7 +134,8 @@ exclude_patterns = []
 # show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+# pygments_style = 'sphinx'
+pygments_style = 'monokai'
 
 # A list of ignored prefixes for module index sorting.
 # modindex_common_prefix = []
