42c625dbb514e3b8d47382632252d296603f57d5
Gregor von Laszewski <laszewski@gmail.com>
2017-01-07 11:32:58 -0500

    fix;dev: remove setup py when creating a new version

--- bin/new_version.sh
+++ bin/new_version.sh
@@ -7,7 +7,7 @@ git tag $TAG
 echo $TAG
 echo "__version__ = \"$TAG\"" > docs/source/version.py
 git commit -m "version $TAG" docs/source/version.py
-python setup.py install
+# python setup.py install
 git commit -m $TAG --allow-empty
 git push origin --tags
 git push
